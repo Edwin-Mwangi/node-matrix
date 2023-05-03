5903d622bb654756eb49b156d1b3d95bfaa74800
Edwin-Mwangi <edwinmwangi138@gmail.com>
2023-05-03 23:00:20 +0300

    preparin for db

--- app.js
+++ app.js
@@ -1,45 +1,22 @@
 //using express
-const express = require('express');//returns a func
-const app = express();//instance of that func given varname app
+const express = require('express');
+const app = express();
+const morgan = require('morgan'); 
 
 //set ejs view engine
 app.set('view engine', 'ejs');
 
-//by default assumes views are in views folder..to set another folder
-// app.set('views','newviews');
-
 //listen for requests
-//no need for localhost to be defined
 app.listen('3000')
 
-//middleware (works top to btm)
-//add arg next() for the code to continue
-app.use((req, res, next) => {
-    console.log('new request made');
-    console.log('hostname: ', req.hostname);
-    console.log('path: ',req.path);
-    console.log('method',req.method);
-    next(); //allows code to continue,get URL and fire correct handler
-})
+//middleware to specify static files
+app.use(express.static('public'))
+
+//running morgan
+app.use(morgan('dev'))
 
 //listen for get requests
 app.get('/',(req,res)=>{
-    // res.write()
-    // res.end()
-    //simpler alternative
-    //send() infers status codes and headers...setHeader() & statusCode="" unrequired
-    // res.send('<p>Express routes to Zion</p>')
-
-    //sending HTML files
-    //sendFile takes in abs dir for rel dir include 2nd arg to show root of relative dir 
-    //__dirname gets that Abs dir that's root of 1st arg.../views & app.js share same dir
-    // res.sendFile('./views/index.html', { root: __dirname})
-
-    //rendering ejs templates
-    //no need for .ejs ext,it looks in views folder and finds index.ejs 
-    //2nd arg is data passed from app.js to ejs templates...check <title>
-    // res.render('index',{ title: 'home'})
-
     //can also pass more complex data
     const blogs = [{ title: 'Mario rides', snippet: ' Lorem ipsum dolor sit amet consectetur.'},
                     { title: 'Luigi follows', snippet: ' Lorem ipsum dolor sit amet consectetur.'},
@@ -49,27 +26,15 @@ app.get('/',(req,res)=>{
     res.render('index',{ title: 'home', blogs})
 })
 
-//another middleware testing topdown approach
-// req, res, included otherwise next not working
-app.use((req, res,next) => {
-    console.log('2nd middleware not fired when we goto "/"');
-    next();
-})
 
 //about page
 app.get('/about',(req,res)=>{
-    // res.send('<p>About Express </p>')
-    //sending HTML files
-    // res.sendFile('./views/about.html', { root: __dirname})
-
-    //ejs
     res.render('about',{ title: 'about'})
 })
 
 //redirects
 app.get('/about-us',(req,res)=>{
     res.redirect('/about')
-     //statuscode 301 included automatically
 });
 
 //blogs/create handler
@@ -78,14 +43,6 @@ app.get('/blogs/create',(req,res)=>{
 });
 
 //404
-//use() used below all others...when above urls dont match
 app.use((req,res)=>{
-    // res.sendFile('./views/404.html', { root: __dirname})
-
-    //to include statuscodes...use() is general for any HTML you wanna send
-    //Express doesn't know its a 404...res.status() returns res obj so can attach senFile()
-    // res.status(404).sendFile('./views/404.html', { root: __dirname})
-
-    //ejs
     res.status(404).render('404',{ title: '404'})
 })
